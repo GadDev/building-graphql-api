ce1a1a56b0aee81459564d56853e5abaa75a1c99
algadaix <alexandre@qured.com>
2020-11-10 22:30:17 +0000

    modularize schema

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const dotEnv = require('dotenv');
 
 //resolvers
 const resolvers = require('./resolvers');
+//Schema
+const typeDefs = require('./typeDefs');
+
 dotEnv.config();
 
 const app = express();
@@ -14,39 +17,6 @@ app.use(cors());
 
 //body parser middleware
 app.use(express.json());
-
-//Schema
-const typeDefs = gql`
-	# Root level type
-	type Query {
-		greetings: String!
-		tasks: [Task!]
-		task(id: ID!): Task
-		users: [User!]
-		user(id: ID!): User
-	}
-	input createTaskInput {
-		name: String!
-		completed: Boolean!
-		userId: ID!
-	}
-	type Mutation {
-		createTask(input: createTaskInput!): Task
-	}
-	# Field type
-	type User {
-		id: ID!
-		name: String!
-		email: String!
-		tasks: [Task!]
-	}
-	type Task {
-		id: ID!
-		name: String!
-		completed: Boolean!
-		user: User!
-	}
-`;
 
 const apolloServer = new ApolloServer({
 	typeDefs,
